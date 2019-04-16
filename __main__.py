71ead628de1ca5a342430709a9f467b090594064
degenerat3 <james.maskelony@gmail.com>
2019-04-15 22:32:36 -0400

    remove print debug

--- __main__.py
+++ __main__.py
@@ -25,7 +25,6 @@ def read_module_code(filename):
         with zipfile.ZipFile(sys.argv[0]) as z:
             try:
                 result = z.read(filename).decode()
-                print(result)
             except:
                 result = z.read(filename)
     else:
